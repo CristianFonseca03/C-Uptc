a3e29e51965fddf2ce7412a5c95863e97e6c8d42
cristian <cristian@cristian-X450LD>
2018-03-05 19:34:09 -0500

    1.0 Prueba

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: cristian
+ *
+ * Created on 5 de marzo de 2018, 19:32
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hola Mundo" << endl;
+
+    return 0;
+
+}
+
